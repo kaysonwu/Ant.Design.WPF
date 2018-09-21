603cc77f4c80d7c09ce6d2bbcee967f0dac12432
junliuxian <772663139@qq.com>
2018-09-21 22:22:45 +0800

    add radio style

--- src/Ant.Design.WPF/Ant.Design.WPF.Shared/Helpers/ControlHelper.cs
+++ src/Ant.Design.WPF/Ant.Design.WPF.Shared/Helpers/ControlHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Antd.Helpers
 {
@@ -8,9 +10,14 @@ namespace Antd.Helpers
     /// </summary>
     public static class ControlHelper
     {
-        public static readonly DependencyProperty CornerRadiusProperty =
-            DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(ControlHelper), new FrameworkPropertyMetadata(new CornerRadius(),
-                FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "CornerRadius", 
+            typeof(CornerRadius), 
+            typeof(ControlHelper), 
+            new FrameworkPropertyMetadata(
+                new CornerRadius(), 
+                FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender
+            ));
 
         /// <summary> 
         /// The CornerRadius property allows users to control the roundness of the button corners independently by 
@@ -27,6 +34,36 @@ namespace Antd.Helpers
         public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
         {
             obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        public static readonly DependencyProperty MouseOverBorderBrushProperty = DependencyProperty.RegisterAttached(
+            "MouseOverBorderBrush", 
+            typeof(Brush), 
+            typeof(ControlHelper), 
+            new FrameworkPropertyMetadata(
+                Brushes.Transparent, 
+                FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits));
+
+        /// <summary>
+        /// Gets the brush used to draw the focus border.
+        /// </summary>
+        [Category(DesignerConstants.LibraryName)]
+        [AttachedPropertyBrowsableForType(typeof(TextBox))]
+        [AttachedPropertyBrowsableForType(typeof(CheckBox))]
+        [AttachedPropertyBrowsableForType(typeof(RadioButton))]
+        [AttachedPropertyBrowsableForType(typeof(DatePicker))]
+        [AttachedPropertyBrowsableForType(typeof(ComboBox))]
+        public static Brush GetMouseOverBorderBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(MouseOverBorderBrushProperty);
+        }
+
+        /// <summary>
+        /// Sets the brush used to draw the mouse over brush.
+        /// </summary>
+        public static void SetMouseOverBorderBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(MouseOverBorderBrushProperty, value);
         }
 
     }
