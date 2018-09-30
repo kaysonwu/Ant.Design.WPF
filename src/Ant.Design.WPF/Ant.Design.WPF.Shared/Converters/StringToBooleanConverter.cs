e18551326c99499b3cf8815f0f8ba391ff5d9217
junliuxian <772663139@qq.com>
2018-09-30 09:53:23 +0800

    complete button icon style

---
+++ src/Ant.Design.WPF/Ant.Design.WPF.Shared/Converters/StringToBooleanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Antd.Converters
+{
+    public class StringToBooleanConverter : IValueConverter
+    {
+        private static StringToBooleanConverter instance;
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static StringToBooleanConverter() { }
+
+        private StringToBooleanConverter() { }
+
+        public static StringToBooleanConverter Instance
+        {
+            get { return instance ?? (instance = new StringToBooleanConverter()); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
