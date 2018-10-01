1d2f68c53e0a645c9b45ae3cd1e2155638ce89db
junliuxian <772663139@qq.com>
2018-10-01 22:56:59 +0800

    add checkbox demo

---
+++ src/Ant.Design.WPF.Samples/Ant.Design.WPF.Demo/Ant.Design.WPF.Demo.Shared/ViewModels/CheckBoxViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Caliburn.Micro;
+
+namespace AntdDemo.ViewModels
+{
+    [Export(typeof(IScreen))]
+    internal class CheckBoxViewModel : Screen
+    {
+        public CheckBoxViewModel()
+        {
+            DisplayName = "CheckBox";
+        }
+    }
+}
