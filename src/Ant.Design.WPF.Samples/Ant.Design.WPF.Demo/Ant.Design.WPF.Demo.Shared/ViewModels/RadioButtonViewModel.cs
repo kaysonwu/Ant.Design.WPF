1abcf67d37fff851149a58a5c09a65c45aa7c6b7
junliuxian <772663139@qq.com>
2018-10-04 19:56:29 +0800

    1.optimize radio style 2.add radio demo

---
+++ src/Ant.Design.WPF.Samples/Ant.Design.WPF.Demo/Ant.Design.WPF.Demo.Shared/ViewModels/RadioButtonViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+
+namespace AntdDemo.ViewModels
+{
+    [Export(typeof(IScreen))]
+    internal class RadioButtonViewModel : Screen
+    {
+        private bool isEnabled;
+
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set {
+                isEnabled = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+
+        public RadioButtonViewModel()
+        {
+            DisplayName = "Radio";
+        }
+
+        public void Toggle()
+        {
+            IsEnabled = !isEnabled;
+        }
+    }
+}
