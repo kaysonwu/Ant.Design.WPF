c9fd49886e21ebafde1b86ba059229a223b52a10
junliuxian <772663139@qq.com>
2018-10-26 22:37:34 +0800

    Add Icon demo

---
+++ src/Ant.Design.WPF.Samples/Ant.Design.WPF.Demo/Ant.Design.WPF.Demo.Shared/ViewModels/IconViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Caliburn.Micro;
+
+namespace AntdDemo.ViewModels
+{
+    [Export(typeof(IScreen))]
+    public class IconViewModel : Screen
+    {
+        public IconViewModel()
+        {
+            DisplayName = "Icon";
+        }
+    }
+}
