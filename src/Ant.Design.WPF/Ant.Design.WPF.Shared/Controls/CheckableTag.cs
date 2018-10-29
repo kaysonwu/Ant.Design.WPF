0f68ddd1c96f0857f3e1d5bbaae44c3a42f7656b
junliuxian <772663139@qq.com>
2018-10-29 11:06:43 +0800

    add CheckableTag control

---
+++ src/Ant.Design.WPF/Ant.Design.WPF.Shared/Controls/CheckableTag.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Antd.Controls
+{
+    /// <summary>
+    /// CheckableTag works like Checkbox, click it to toggle checked.
+    /// </summary>
+    public class CheckableTag : ToggleButton
+    {
+        static CheckableTag()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckableTag), new FrameworkPropertyMetadata(typeof(CheckableTag)));
+        }
+    }
+}
