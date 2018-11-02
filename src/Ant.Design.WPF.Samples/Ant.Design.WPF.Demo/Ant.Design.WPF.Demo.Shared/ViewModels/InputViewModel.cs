143bc8985dc9cbea6635d746a083d940ab6944a5
junliuxian <772663139@qq.com>
2018-11-02 18:37:03 +0800

    add input demo

---
+++ src/Ant.Design.WPF.Samples/Ant.Design.WPF.Demo/Ant.Design.WPF.Demo.Shared/ViewModels/InputViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+using Caliburn.Micro;
+
+namespace AntdDemo.ViewModels
+{
+    [Export(typeof(IScreen))]
+    internal class InputViewModel : Screen
+    {
+        public InputViewModel()
+        {
+            DisplayName = "Input";
+        }
+    }
+}
