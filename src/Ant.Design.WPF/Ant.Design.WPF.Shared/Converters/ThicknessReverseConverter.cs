d580b1709eb34d72e39e4427bab419e17dea75ba
junliuxian <772663139@qq.com>
2018-11-11 18:34:52 +0800

    update textbox style

---
+++ src/Ant.Design.WPF/Ant.Design.WPF.Shared/Converters/ThicknessReverseConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Antd.Converters
+{
+    [ValueConversion(typeof(Thickness), typeof(Thickness))]
+    public class ThicknessReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var thickness = (Thickness)value;
+
+            thickness.Left *= -1;
+            thickness.Top *= -1;
+
+            thickness.Right *= -1;
+            thickness.Bottom *= -1;
+
+            return thickness;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
