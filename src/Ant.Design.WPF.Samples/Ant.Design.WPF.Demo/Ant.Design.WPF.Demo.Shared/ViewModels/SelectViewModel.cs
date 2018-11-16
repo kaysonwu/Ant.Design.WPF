841f3dc9d6504e45a4421f216e95302ba4d202bb
junliuxian <772663139@qq.com>
2018-11-16 23:14:43 +0800

    add Select demo

---
+++ src/Ant.Design.WPF.Samples/Ant.Design.WPF.Demo/Ant.Design.WPF.Demo.Shared/ViewModels/SelectViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Caliburn.Micro;
+
+namespace AntdDemo.ViewModels
+{
+    [Export(typeof(IScreen))]
+    internal class SelectViewModel : Screen
+    {
+        public SelectViewModel()
+        {
+            DisplayName = "Select";
+        }
+    }
+}
