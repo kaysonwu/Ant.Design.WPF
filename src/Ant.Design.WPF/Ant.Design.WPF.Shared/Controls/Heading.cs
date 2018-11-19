37687340764d179c0b2f77a95d55aea8d15cb127
junliuxian <772663139@qq.com>
2018-11-19 12:01:03 +0800

    add heading

---
+++ src/Ant.Design.WPF/Ant.Design.WPF.Shared/Controls/Heading.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Antd.Controls
+{
+    /// <summary>
+    /// A heading of the a page.
+    /// </summary>
+    public class Heading : TextBlock
+    {
+        #region Properties
+
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register("Size", typeof(HeadingSizes), typeof(Heading), new PropertyMetadata(HeadingSizes.Normal));
+
+        /// <summary>
+        /// Get or set the size of the heading.
+        /// </summary>
+        public HeadingSizes Size
+        {
+            get { return (HeadingSizes)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        static Heading()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Heading), new FrameworkPropertyMetadata(typeof(Heading)));
+        }
+
+        #endregion
+    }
+
+    public enum HeadingSizes : byte
+    {
+       ExtraLarge, Large, Normal, Medium, Small, Tiny
+    }
+}
