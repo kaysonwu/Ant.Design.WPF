710355bbde8d151bf0a186745a4b3260d39bb795
junliuxian <772663139@qq.com>
2018-11-19 18:15:13 +0800

    add Hyperlink

---
+++ src/Ant.Design.WPF/Ant.Design.WPF.Shared/Controls/Hyperlink.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Antd.Controls
+{
+    /// <summary>
+    /// A hyperlink button.
+    /// </summary>
+    public class Hyperlink : ButtonBase
+    {
+
+        #region Properties
+
+        public static readonly DependencyProperty UriProperty =
+            DependencyProperty.Register("Uri", typeof(string), typeof(Hyperlink), new PropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// Gets or sets the uri of hyperlinks.
+        /// </summary>
+        public string Uri
+        {
+            get { return (string)GetValue(UriProperty); }
+            set { SetValue(UriProperty, value); }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        static Hyperlink()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Hyperlink), new FrameworkPropertyMetadata(typeof(Hyperlink)));
+        }
+        #endregion
+    }
+}  
