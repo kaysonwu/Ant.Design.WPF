0abb93bbe3077332ffd93c6c471ac9b29b8a73d1
junliuxian <772663139@qq.com>
2019-09-02 23:25:05 +0800

    add TabControl

---
+++ src/Ant.Design.WPF/Ant.Design.WPF.Shared/Controls/TabControl.cs
@@ -0,0 +1,17 @@
+﻿namespace Antd.Controls
+{
+    using System.Windows;
+    using TabControlBase = System.Windows.Controls.TabControl;
+
+    public class TabControl : TabControlBase
+    {
+        #region Constructors
+
+        static TabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TabControl), new FrameworkPropertyMetadata(typeof(TabControl)));
+        }
+
+        #endregion
+    }
+}
